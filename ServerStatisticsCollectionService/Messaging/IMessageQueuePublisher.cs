ccdd9d90a6907cca851384e285371885cf1155dc
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-28 17:52:21 -0300

    Add Interface IMessageQueuePublisher

---
+++ ServerStatisticsCollectionService/Messaging/IMessageQueuePublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatisticsCollectionService.Messaging
+{
+    public interface IMessageQueuePublisher
+    {
+        void Publish<T>(string topic, T message);
+    }
+}
