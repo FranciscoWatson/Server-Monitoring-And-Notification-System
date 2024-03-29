6e3896fa7a0dc03911df0ecbcbfe7baa2d9d34d7
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-29 12:46:42 -0300

    Add Factories for ServerStatisticCollector and MessageQuePublisher

---
+++ ServerStatisticsCollectionService/MessageQueuePublisherFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using ServerStatisticsCollectionService.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatisticsCollectionService
+{
+    public class MessageQueuePublisherFactory
+    {
+        public IMessageQueuePublisher Create(IConfiguration configuration)
+        {
+            var hostName = configuration.GetSection("RabbitMQConfig")["HostName"];
+            var port = Convert.ToInt32(configuration.GetSection("RabbitMQConfig")["Port"]);
+            var userName = configuration.GetSection("RabbitMQConfig")["UserName"];
+            var password = configuration.GetSection("RabbitMQConfig")["Password"];
+
+            return new RabbitMqPublisher(hostName, port, userName, password);
+        }
+    }
+}
---
+++ ServerStatisticsCollectionService/ServerStatisticsCollectorFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using ServerStatisticsCollectionService.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatisticsCollectionService
+{
+    public class ServerStatisticsCollectorFactory
+    {
+        public ServerStatisticsCollector Create(IConfiguration configuration, IMessageQueuePublisher messageQueuePublisher)
+        {
+            var samplingIntervalSeconds = Convert.ToInt32(configuration.GetSection("ServerStatisticsConfig")["SamplingIntervalSeconds"]);
+            var serverIdentifier = configuration.GetSection("ServerStatisticsConfig")["ServerIdentifier"];
+
+            return new ServerStatisticsCollector(serverIdentifier, samplingIntervalSeconds, messageQueuePublisher);
+        }
+    }
+}
