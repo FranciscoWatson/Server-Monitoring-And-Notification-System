7cf4722ca09426cc0ce3526c941653891e78e929
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-29 12:49:46 -0300

    Add new functionality to the program.cs

--- ServerStatisticsCollectionService/Program.cs
+++ ServerStatisticsCollectionService/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using ServerStatisticsCollectionService;
+
+
+var configuration = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json")
+.Build();
+
+var messageQueuePublisherFactory = new MessageQueuePublisherFactory();
+var messageQueuePublisher = messageQueuePublisherFactory.Create(configuration);
+
+
+var serverStatisticsCollectorFactory = new ServerStatisticsCollectorFactory();
+var statisticsCollectionService = serverStatisticsCollectorFactory.Create(configuration, messageQueuePublisher);
+
+
+statisticsCollectionService.StartAsync();
+
+Console.ReadKey();
