3c9a2c78f14f6837128ca9f96f1f92c40922eb38
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-01 09:10:53 -0300

    Add Models dir

--- MessageProcessingService/Models/ServerStatistics.cs
+++ MessageProcessingService/Models/ServerStatistics.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MessageProcessingService
+namespace MessageProcessingService.Models
 {
     public class ServerStatistics
     {
