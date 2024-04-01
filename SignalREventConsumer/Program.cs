99a39a90b2882ffcc4f484d526bbf22d1dd66ddf
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-01 17:20:32 -0300

    Add functionality to get the messages on SignalR consumer project

--- SignalREventConsumer/Program.cs
+++ SignalREventConsumer/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+var signalRUrl = configuration.GetSection("SignalRConfig")["SignalRUrl"];
+
+var hubConnection = new HubConnectionBuilder()
+           .WithUrl(signalRUrl)
+           .WithAutomaticReconnect()
+           .Build();
+
+hubConnection.On<string>("ReceiveAlert", (message) =>
+{
+    Console.WriteLine($"Received message: {message}");
+});
+
+
+try
+{
+    await hubConnection.StartAsync();
+    Console.WriteLine("Connected to SignalR hub.");
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while connecting to the SignalR hub: {ex.Message}");
+}
+finally
+{
+    await hubConnection.DisposeAsync();
+}
