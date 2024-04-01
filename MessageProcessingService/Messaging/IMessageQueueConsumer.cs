e5a7fc5b9003792b63c636937c12bd37f7a086cf
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-01 09:14:41 -0300

    Add IMessageQueueConsumer Interface

---
+++ MessageProcessingService/Messaging/IMessageQueueConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MessageProcessingService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageProcessingService.Messaging
+{
+    public interface IMessageQueueConsumer
+    {
+        void Consume(string topic);
+    }
+}
