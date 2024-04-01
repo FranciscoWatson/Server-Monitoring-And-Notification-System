c8a3492b74eb685c8cebb946ad342083a63159a0
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-01 14:11:45 -0300

    Add serverIdentifier to the ServerStatistics model

--- MessageProcessingService/Models/ServerStatistics.cs
+++ MessageProcessingService/Models/ServerStatistics.cs
@@ -8,6 +8,7 @@ namespace MessageProcessingService.Models
 {
     public class ServerStatistics
     {
+        public string ServerIdentifier { get; set; }
         public double MemoryUsage { get; set; } // in MB
         public double AvailableMemory { get; set; } // in MB
         public double CpuUsage { get; set; }
