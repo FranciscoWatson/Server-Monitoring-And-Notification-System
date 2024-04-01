0e9ec00054dad80048f21a3ae19059b03aa7f6a9
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-01 09:16:45 -0300

    Add new consumer functionality to program.cs

--- MessageProcessingService/Program.cs
+++ MessageProcessingService/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using MessageProcessingService;
+using MessageProcessingService.Messaging;
+using MessageProcessingService.Models;
+using MessageProcessingService.Repository;
+using Microsoft.Extensions.Configuration;
+
+
+var configuration = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json")
+.Build();
+
+var hostName = configuration.GetSection("RabbitMQConfig")["HostName"];
+var port = Convert.ToInt32(configuration.GetSection("RabbitMQConfig")["Port"]);
+var userName = configuration.GetSection("RabbitMQConfig")["UserName"];
+var password = configuration.GetSection("RabbitMQConfig")["Password"];
+
+var connectionString = configuration.GetSection("ConnectionStrings")["MongoDBConnection"];
+
+
+
+var mongoDbServerStatisticsRepository = new MongoDbServerStatisticsRepository(connectionString);
+var rabbitMqConsumer = new RabbitMqConsumer(hostName, port, userName, password, mongoDbServerStatisticsRepository);
+
+rabbitMqConsumer.Consume("ServerStatistics.*");
+
+
+Console.ReadKey();
