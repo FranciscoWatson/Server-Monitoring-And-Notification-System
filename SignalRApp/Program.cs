3044d7cc32500b222538d238c9acf98d96b5a116
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-08 10:37:36 -0300

    Add SignalRApp webapp DockerFile and Cors

--- SignalRApp/Program.cs
+++ SignalRApp/Program.cs
@@ -6,10 +6,18 @@ builder.Configuration.AddJsonFile("appsettings.json");
 
 
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://example.com"); // Specify the client origin
+    });
+});
 
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCors("CorsPolicy");
 
 app.UseEndpoints(endpoints =>
 {
